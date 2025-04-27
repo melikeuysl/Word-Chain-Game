1453f70654c24c612651452bd6d36d57fd8b3e95
Melike Uysal <melikeyusl1103@gmail.com>
2025-04-27 16:32:33 +0300

    Update README and routing for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+//import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+// NOTE: GitHub Pages'de sayfa yenileme sorunlarını önlemek için HashRouter kullanıldı.
 import Navbar from './components/Navbar';
 import Game from './components/Game';
 import Rules from './components/Rules';
@@ -11,7 +13,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Navigate to="/game" />} />
+        <Route path="/" element={<Navigate to="/game" replace />} />
         <Route path="/game" element={<Game />} />
         <Route path="/rules" element={<Rules />} />
         <Route path="/about" element={<GameInfo />} />
